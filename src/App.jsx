c8778d94be5722167c2e566be458ebe7c038628e
Aung Pyae Hein <aungpyaehein917@gmail.com>
2026-03-12 22:19:40 +0630

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-export default function App(){
+function App() {
   return (
-      <h1 className="text-4xl font-bold text-blue-600">
-        tailwind
-      </h1>
-  )
+    <div className="border-b-2 border-border flex items-center justify-between">
+      <div className="w-[100px]">
+        <img
+          src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6628074b71d6e1e95593d310_Logo.svg"
+          alt=""
+        />
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Process</a>
+          </li>
+          <li>
+            <a href="#">Integrations</a>
+          </li>
+          <li>
+            <a href="">Pricing</a>
+          </li>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Reviews</a>
+          </li>
+        </ul>
+        <button>Buy Template</button>
+      </div>
+    </div>
+  );
 }
+
+export default App;
