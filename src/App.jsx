4b55b4fdb1c58f4e8d71fdf2175811e3c1787196
Aung Pyae Hein <aungpyaehein917@gmail.com>
2026-03-19 19:33:10 +0630

    .

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ function App() {
       </div>
       <div className="hero-bg-image pb-20 flex padding items-end mt-20 bg-no-repeat bg-[url(https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/665b169cd354209f30160ac5_Square-Shapes.svg)]">
         <div className="w-[50%]">
-          <div className="w-[70%] flex flex-col gap-4">
+          <div className="flex flex-col gap-4 w-[70%]">
             <h1 className="text-6xl leading-[1.1] font-bold">
-              Landing Page Template for
+              Landing Page Template for{" "}
               <span className="text-own-primary">Startups</span>
             </h1>
             <p>
@@ -87,6 +87,168 @@ function App() {
             src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/6665ac32bb1cc3289b9c29a4_PlutoDelux-Logo.svg"
             alt=""
           />
+        </div>
+      </div>
+      <div>
+        <h1>Including Custom-made Illustrations</h1>
+        <p>
+          Our unique illustrations will allow you to explain concepts relevant
+          to SaaS industry in an engaging and fun way.
+        </p>
+        <div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 394 540"
+              width="394"
+              height="540"
+              preserveAspectRatio="xMidYMid meet"
+              style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;"
+            >
+              <defs>
+                <clipPath id="__lottie_element_4">
+                  <rect width="394" height="540" x="0" y="0"></rect>
+                </clipPath>
+              </defs>
+              <g clip-path="url(#__lottie_element_4)">
+                <g
+                  transform="matrix(1,0,0,1,-124,266)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        fill-opacity="0"
+                        stroke="rgb(34,40,49)"
+                        stroke-opacity="1"
+                        stroke-width="11.902"
+                        d=" M325.05999755859375,215.77699279785156 C323.4549865722656,216.57899475097656 340.05999755859375,215.77699279785156 340.05999755859375,215.77699279785156 C340.05999755859375,215.77699279785156 303,215.38900756835938 303,215.38900756835938 C303,215.38900756835938 160.35699462890625,215.38900756835938 160.35699462890625,215.38900756835938 C148.32400512695312,215.38900756835938 139.50100708007812,205.76300048828125 139.50100708007812,194.53199768066406 C139.50100708007812,194.53199768066406 139.50100708007812,-233.03700256347656 139.50100708007812,-233.03700256347656 C139.50100708007812,-245.07000732421875 149.12600708007812,-253.8939971923828 160.35699462890625,-253.8939971923828 C160.35699462890625,-253.8939971923828 373.739990234375,-253.8939971923828 373.739990234375,-253.8939971923828 C373.739990234375,-253.8939971923828 465.19000244140625,-156.0260009765625 465.19000244140625,-156.0260009765625 C465.19000244140625,-156.0260009765625 465.19000244140625,82.2249984741211 465.19000244140625,82.2249984741211"
+                      ></path>
+                    </g>
+                  </g>
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        fill-opacity="0"
+                        stroke="rgb(34,40,49)"
+                        stroke-opacity="1"
+                        stroke-width="11.902"
+                        d=" M372.93798828125,-249.08099365234375 C372.93798828125,-249.08099365234375 372.93798828125,-156.0260009765625 372.93798828125,-156.0260009765625 C372.93798828125,-156.0260009765625 463.58599853515625,-156.0260009765625 463.58599853515625,-156.0260009765625"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+                <g
+                  transform="matrix(1,0,0,1,-124.00000762939453,270)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      fill-opacity="0"
+                      stroke="rgb(34,40,49)"
+                      stroke-opacity="1"
+                      stroke-width="11.902"
+                      d=" M195.6540069580078,-82.42500305175781 C195.6540069580078,-82.42500305175781 414.6520080566406,-82.42500305175781 414.6520080566406,-82.42500305175781"
+                    ></path>
+                  </g>
+                </g>
+                <g
+                  transform="matrix(1,0,0,1,-124,270)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      fill-opacity="0"
+                      stroke="rgb(34,40,49)"
+                      stroke-opacity="1"
+                      stroke-width="11.902"
+                      d=" M195.6540069580078,-7.513999938964844 C195.6540069580078,-7.513999938964844 414.6520080566406,-7.513999938964844 414.6520080566406,-7.513999938964844"
+                    ></path>
+                  </g>
+                </g>
+                <g
+                  transform="matrix(1,0,0,1,-124,270)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      fill-opacity="0"
+                      stroke="rgb(34,40,49)"
+                      stroke-opacity="1"
+                      stroke-width="11.902"
+                      d=" M195.88999938964844,72.7030029296875 C195.88999938964844,72.7030029296875 305.3890075683594,72.7030029296875 305.3890075683594,72.7030029296875"
+                    ></path>
+                  </g>
+                </g>
+                <g
+                  transform="matrix(1,0,0,1,-124,270)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      fill-opacity="0"
+                      stroke="rgb(121,113,234)"
+                      stroke-opacity="1"
+                      stroke-width="11.902"
+                      d=" M418.50299072265625,72.5979995727539 C468.5669860839844,72.5979995727539 509.1510009765625,113.18299865722656 509.1510009765625,163.24600219726562 C509.1510009765625,213.3090057373047 468.5669860839844,253.8939971923828 418.50299072265625,253.8939971923828 C368.4389953613281,253.8939971923828 327.8550109863281,213.3090057373047 327.8550109863281,163.24600219726562 C327.8550109863281,113.18299865722656 368.4389953613281,72.5979995727539 418.50299072265625,72.5979995727539z"
+                    ></path>
+                  </g>
+                </g>
+                <g
+                  transform="matrix(1,0,0,1,-124,270)"
+                  opacity="1"
+                  style="display: block;"
+                >
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        fill-opacity="0"
+                        stroke="rgb(121,113,234)"
+                        stroke-opacity="1"
+                        stroke-width="11.902"
+                        d=" M418.6629943847656,118.8239974975586 C418.6629943847656,118.8239974975586 418.6629943847656,207.06500244140625 418.6629943847656,207.06500244140625"
+                      ></path>
+                    </g>
+                  </g>
+                  <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                    <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        fill-opacity="0"
+                        stroke="rgb(121,113,234)"
+                        stroke-opacity="1"
+                        stroke-width="11.902"
+                        d=" M462.3580017089844,163.1739959716797 C462.3580017089844,163.1739959716797 374.11700439453125,163.1739959716797 374.11700439453125,163.1739959716797"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <p>Easy Setup</p>
+            <p>Get going in days not months with our template.</p>
+          </div>
         </div>
       </div>
     </>
