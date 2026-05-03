cef9c8d8fff0c1e1720cdf2f0fb1b4eeee88bd26
Aung-Pyae-Hein <aungpyaehein917@gmail.com>
2026-05-03 15:24:25 +0630

    responsive for all screen size of hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,10 @@ function App() {
           <Menu size={30} className="lg:hidden block" />
         </div>
       </div>
-      <div className="hero-bg-image pb-20 flex padding items-end mt-20 bg-no-repeat bg-[url(https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/665b169cd354209f30160ac5_Square-Shapes.svg)]">
-        <div className="w-[50%]">
+      <div className="hero-bg-image flex md:flex-row flex-col md:gap-0 gap-8 padding lg:items-end items-start pb-20 mt-20 bg-no-repeat md:bg-[url(https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/665b169cd354209f30160ac5_Square-Shapes.svg)]">
+        <div className="md:w-[50%] w-full">
           <div className="flex flex-col gap-4 xl:w-[70%] w-full">
-            <h1 className="text-6xl leading-[1.1] font-bold">
+            <h1 className="lg:text-6xl text-5xl leading-[1.1] font-bold">
               Landing Page Template for{" "}
               <span className="text-own-primary">Startups</span>
             </h1>
@@ -49,13 +49,17 @@ function App() {
               Use our template to quickly and efficiently brand your growing
               business.
             </p>
-            <div className="flex items-center gap-3">
-              <button className="btn !px-8 !py-4">Preview Template</button>
-              <button className="outline-btn !px-8 !py-4">Learn More</button>
+            <div className="flex md:flex-row flex-col items-center flex-grow gap-3">
+              <button className="btn !px-8 !py-4 md:basis-auto shrink-0 md:w-auto w-full">
+                Preview Template
+              </button>
+              <button className="outline-btn !px-8 !py-4 md:basis-auto shrink-0 md:w-auto w-full">
+                Learn More
+              </button>
             </div>
           </div>
         </div>
-        <div className="w-[50%]">
+        <div className="md:w-[50%] w-full">
           <img
             src="https://cdn.prod.website-files.com/6627e9a3eba2a9009dbdeffe/662a5c31b674b313da821db0_Hero-Illustration.svg"
             alt=""
@@ -63,7 +67,7 @@ function App() {
         </div>
       </div>
       <div className="padding">
-        <h1 className="text-lg font-semibold text-center">
+        <h1 className="text-xl font-semibold text-left lg:text-center">
           More than <span className="text-own-primary">1000+</span> Companies
           using our template
         </h1>
@@ -95,25 +99,25 @@ function App() {
         </div>
       </div>
       <div className="padding mt-[150px]">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-3xl md:text-4xl font-semibold md:font-bold">
           Including Custom-made Illustrations
         </h1>
-        <p className="w-[40%] mt-2">
+        <p className="w-full md:w-[70%] lg:w-[60%] xl:w-[40%] mt-2">
           Our unique illustrations will allow you to explain concepts relevant
           to SaaS industry in an engaging and fun way.
         </p>
-        <div className="grid grid-cols-3 gap-5 mt-10">
-          <div className="border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-10">
+          <div className="hover:border-own-primary transition-all border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
             <FilePlusCorner size={42} />
             <p className="text-2xl font-bold">Easy Setup</p>
             <p>Get going in days not months with our template.</p>
           </div>
-          <div className="border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
+          <div className="hover:border-own-primary transition-all border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
             <CircleDollarSign size={42} />
             <p className="text-2xl font-bold">Flexible Pricing</p>
             <p>Find the right pricing plan for your custom needs.</p>
           </div>
-          <div className="border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
+          <div className="hover:border-own-primary transition-all border-[3px] border-border rounded-2xl p-8 flex flex-col gap-3">
             <ShieldPlus size={42} />
             <p className="text-2xl font-bold">Secure Solution</p>
             <p>Highest security standards will provide a peace of mind.</p>
